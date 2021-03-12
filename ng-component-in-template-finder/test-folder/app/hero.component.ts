6445c5f434ca202a4c7b6c13b9ec38aaba08cd63
Aaron Bery <aaron.bery@landg.com>
2021-03-12 13:24:11 +0000

    feat(program): create app

---
+++ ng-component-in-template-finder/test-folder/app/hero.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+    selector: 'ma-hero',
+    templateUrl: 'hero.component.html'
+})
+
+export class HeroComponent implements OnInit {
+    @Input() title = 'Hello world';
+
+    constructor() { }
+
+    ngOnInit() { }
+}
---
+++ ng-component-in-template-finder/test-folder/app/sidebar.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+    selector: 'ma-sidebar',
+    templateUrl: 'sidebar.component.html'
+})
+
+export class SidebarComponent implements OnInit {
+    @Input() title = 'Hello world';
+
+    constructor() { }
+
+    ngOnInit() { }
+}
