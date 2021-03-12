464a5b868988c271a21c673ba678d2c320447195
Aaron Bery <aaron.bery@landg.com>
2021-03-12 13:24:11 +0000

    feat(program): create app

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# ng-component-in-template-finder
+# Ng Component in template finder
 Finds components in an angular app that are not in use in order to facilitate app maintenance
+
+## Install
+Dotnet core 3.1 needs to be installed on your system. Dotnet core is available on Windows, Mac OS and Linux from Microsoft
+https://dotnet.microsoft.com/download
+
+## Run
+The application is run using the dotnet from the command line/terminal. You need to pass the directories of the Angular app you want to scan as an argument e.g.
+`dotnet run -- /Users/joebloggs/Projects/angular-app/src`
+
+The unused components are returned in the console as their angular selector value.
---
+++ ng-component-in-template-finder/Program.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ng_component_in_template_finder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                Process(args[0], args[0]);
+            }
+        }
+
+        static void ProcessFiles(string[] files, string rootDirectory)
+        {
+            try
+            {
+                foreach (string file in files)
+                {
+                    if (IsComponentFile(file))
+                    {
+                        var selector = GetComponentSelectorInFile(file);
+                        if (selector != null && !IsSelectorUsed(selector, rootDirectory))
+                        {
+                            Console.WriteLine($"Selector {selector} is not in use");
+                        }
+                    }
+                }
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("There was an error processing file:", error.Message);
+            }
+        }
+
+        static bool IsHtmlFile(string fileName)
+        {
+            return Regex.Match(fileName, "^.+\\.html$").Success;
+        }
+
+        static bool IsComponentFile(string fileName)
+        {
+            return Regex.Match(fileName, "^.+\\.component\\.ts").Success;
+        }
+
+
+        static void Process(string path, string rootDirectory)
+        {
+            try
+            {
+                string[] directories = Directory.GetDirectories(path);
+
+                string[] files = Directory.GetFiles(path);
+                
+                if (files.Length > 0)
+                {
+                    ProcessFiles(files, rootDirectory);
+                }
+
+                foreach (string directory in directories)
+                {
+                    if (Directory.Exists(directory))
+                    {
+                        Process(directory, rootDirectory);
+                    }
+                }
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("There was an error processing:", error.Message);
+            }
+        }
+
+        static bool IsSelectorUsed(string selector, string rootDirectory)
+        {
+            bool isUsed = false;
+
+            string[] directories = Directory.GetDirectories(rootDirectory);
+
+            string[] files = Directory.GetFiles(rootDirectory);
+
+            if (IsSelectorUsedInFiles(files, selector))
+            {
+                isUsed = true;
+            }
+
+            foreach (string directory in directories)
+            {
+                if (IsSelectorUsedInDirectory(directory, selector))
+                {
+                    isUsed = true;
+                }
+            }
+
+
+            return isUsed;
+        }
+
+        static bool IsSelectorUsedInDirectory(string path, string selector)
+        {
+            bool isUsed = false;
+
+            try
+            {
+                string[] directories = Directory.GetDirectories(path);
+
+                string[] files = Directory.GetFiles(path);
+
+                if (IsSelectorUsedInFiles(files, selector))
+                {
+                    isUsed = true;
+                }
+
+                foreach (string directory in directories)
+                {
+                    if (IsSelectorUsedInDirectory(directory, selector))
+                    {
+                        isUsed = true;
+                    }
+                }
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("There was an error processing:", error.Message);
+            }
+
+            return isUsed;
+        }
+
+        static bool IsSelectorUsedInFiles(string[] files, string selector)
+        {
+            bool isUsed = false;
+
+            try
+            {
+
+                foreach (string file in files)
+                {
+                    if (IsHtmlFile(file))
+                    {
+                        // Open the text file using a stream reader.
+                        using (var sr = new StreamReader(file))
+                        {
+                            string closingTag = $"</{selector}>";
+                            MatchCollection matchedSelectors = Regex.Matches(sr.ReadToEnd(), closingTag);
+
+                            if (matchedSelectors.Count > 0)
+                            {
+                                isUsed = true;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("There was an error in IsSelectorUsed:", error.Message);
+            }
+
+            return isUsed;
+        }
+
+        static string GetComponentSelectorInFile(string path)
+        {
+            string selector = null;
+
+            try
+            {
+                // Open the text file using a stream reader.
+                using (var sr = new StreamReader(path))
+                {
+                    MatchCollection matchedSelectors = Regex.Matches(sr.ReadToEnd(), "selector: '[^']+'");
+
+                    if (matchedSelectors.Count > 0)
+                    {
+                        selector = Regex.Replace(matchedSelectors[0].ToString(), "selector: '([^']+)'", "$1");
+                    }
+                }
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("There was an error in GetComponentSelectorInFile:", error.Message);
+            }
+
+            return selector;
+        }
+
+
+    }
+}
