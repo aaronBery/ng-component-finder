2592bb045338d94bcdbc05b3e9896eeb646b6af9
Aaron Bery <7768665+aaronBery@users.noreply.github.com>
2021-08-11 16:42:57 +0100

    feat(core): find components in routes (#13)

--- ng-component-finder-tests/Test.cs
+++ ng-component-finder-tests/Test.cs
@@ -21,6 +21,24 @@ namespace ng_component_finder_tests
         }
 
         [Test]
+        public void TestIsTsFileTest()
+        {
+            string htmlFile = "../test-folder/app/hero.component.html";
+            string tsFile = "../test-folder/app/hero.component.ts";
+            Assert.False(Program.IsTsFile(htmlFile));
+            Assert.True(Program.IsTsFile(tsFile));
+        }
+
+        [Test]
+        public void TestIsSpecTsFileTest()
+        {
+            string specFile = "../test-folder/app/hero.component.spec.ts";
+            string tsFile = "../test-folder/app/hero.component.ts";
+            Assert.False(Program.IsSpecTsFile(tsFile));
+            Assert.True(Program.IsSpecTsFile(specFile));
+        }
+
+        [Test]
         public void IsComponentFileTest()
         {
             string templateFile = "../test-folder/app/hero.component.html";
@@ -47,10 +65,26 @@ namespace ng_component_finder_tests
         }
 
         [Test]
+        public void IsComponentNameUsedInFilesTest()
+        {
+            const string file = "../../../../ng-component-finder/test-folder/app/app.module.ts";
+            string[] files = new String[] { file };
+            Assert.True(Program.IsComponentNameUsedInRouteConfigInFiles(files, "HeroComponent"));
+            Assert.False(Program.IsComponentNameUsedInRouteConfigInFiles(files, "VillianComponent"));
+        }
+
+        [Test]
         public void GetComponentSelectorInFileTest()
         {
             const string file = "../../../../ng-component-finder/test-folder/app/hero.component.ts";
             Assert.AreEqual("app-hero", Program.GetComponentSelectorInFile(file));
+        }
+
+        [Test]
+        public void GetComponentNameInFileTest()
+        {
+            const string file = "../../../../ng-component-finder/test-folder/app/hero.component.ts";
+            Assert.AreEqual("HeroComponent", Program.GetComponentNameInFile(file));
         }
     }
 }
